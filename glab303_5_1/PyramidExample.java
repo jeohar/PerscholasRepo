eb5a2313094e1bbbaab8d8287eeccc4c1fac0d2c
Hasib Johar <jeohar@outlook.com>
2024-03-28 12:17:55 -0700

    Assignments

---
+++ glab303_5_1/PyramidExample.java
@@ -0,0 +1,19 @@
+package glab303_5_1;
+
+import java.util.Scanner;
+
+public class PyramidExample {
+    public static void main(String[] args) {
+
+
+
+
+        for(int i = 1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
